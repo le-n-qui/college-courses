55e56a37b84d8ee29b7e00dc2fa2506a6b73455a
Andy Qui Le <andy.quile@gmail.com>
2026-03-18 12:25:16 -0700

    Add code to read in user input to question 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
+    // Introduction line
     println!("Hello, please provide course information below!");
+
+    // Ask for the course name
     println!("Course name: ");
+
+    // Create a mutable variable to save user response
+    let mut response1 = String::new()
+
+    // Read user input
+    io::stdin()
+        .read_line(&mut response1)
+        .expect("Failed to read user response.");
+
+    // Ask for the course department
     println!("Course department: ");
+
 }
